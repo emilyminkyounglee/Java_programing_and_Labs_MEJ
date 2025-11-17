9b73287972263dc6c297b6e456c015721c907ab0
Minkyoung Lee <minkyoungemilylee@ewha.ac.kr>
2025-11-17 13:13:04 +0900

    feat: added logs and fixed minor logic issues

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,14 +11,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GameService service = new GameService();
-		GameController controller = new GameController();
 		GameView view = new GameView();
-		
 		Player p1 = new Player();
 		Player p2 = new Player();
+		GameController controller = new GameController(view, service, p1, p2);
 		int role = (int)((int)Math.random()*1000/100);
-		service.setPlayerInfo(p1, role);
-		service.setPlayerInfo(p2, role + 1);
+		
+		controller.runGame();
 		
 		
 	}
